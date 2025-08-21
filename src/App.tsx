7d15ed12d2045c314ba308493e0b869843359bfe
cuong.nguyent <cuong.nguyent@s3corp.com.vn>
2025-08-21 16:14:35 +0700

    feat: config Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className=''>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} className='!bg-gray-600 font-bold'>
           count is {count}
         </button>
         <p>
