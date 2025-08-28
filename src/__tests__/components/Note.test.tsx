8e503f1f1d8fce464212794ac4d72257c4c7a885
cuong.nguyent <cuong.nguyent@s3corp.com.vn>
2025-08-28 15:17:01 +0700

    test: add deleteNote functionality test for NoteCard component

--- src/__tests__/components/Note.test.tsx
+++ src/__tests__/components/Note.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { AuthProvider } from '../../contexts/AuthContext';
 import { NotesProvider } from '../../contexts/NotesContext';
 import NoteCard from '../../components/NoteCard';
@@ -28,5 +28,32 @@ describe('Note Component', () => {
 
     const note = screen.getByText('note 1');
     expect(note).toBeInTheDocument();
+  });
+
+  test('should call deleteNote when delete a note', () => {
+    const mockDeleteNote = jest.fn();
+    const tempId = crypto.randomUUID();
+    render(
+      <AuthProvider>
+        <NotesProvider>
+          <Provider store={store}>
+            <NoteCard
+              title='note 1'
+              content='noi dung note 1'
+              id={tempId}
+              viewNote={() => {}}
+              deleteNote={mockDeleteNote}
+              createdAt={new Date()}
+              updatedAt={new Date()}
+            />
+          </Provider>
+        </NotesProvider>
+      </AuthProvider>
+    );
+
+    const deleteBtn = screen.getByText('Xóa');
+    fireEvent.click(deleteBtn);
+
+    expect(mockDeleteNote).toHaveBeenCalledWith(tempId);
   });
 });
